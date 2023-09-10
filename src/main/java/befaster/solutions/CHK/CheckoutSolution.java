8745a23afa9b55fed87493a21bcf411c9be3b484
sbx_user1051 <sbx_user1051@169.254.21.253>
2023-09-10 19:21:58 +0000

    Sun Sep 10 19:21:58 UTC 2023

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -1,45 +1,42 @@
 package befaster.solutions.CHK;
 
-import befaster.runner.SolutionNotImplementedException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class CheckoutSolution {
 
     Map<Character, Integer> prices = new HashMap<>();
-    Map<Character, Integer> count = new HashMap<>();
+    Map<Character, Integer> skuCount = new HashMap<>();
     public Integer checkout(String skus) {
-        prices.put('A', 50);
-        prices.put('B', 30);
-        prices.put('C', 20);
-        prices.put('D', 15);
+        buildPrices();
 
-        int total = 0;
         for (int i = 0; i < skus.length(); i++) {
             char sku = skus.charAt(i);
             if (!prices.containsKey(sku)){
                 return -1;
             }
-            count.put(sku, count.getOrDefault(sku, 0) + 1);
+            skuCount.put(sku, skuCount.getOrDefault(sku, 0) + 1);
         }
 
-        total += 130 * (count.get('A') / 3);
-        total += 45 * (count.get('B') / 2);
 
-        count.put('A', count.get('A') % 3);
-        count.put('B', count.get('A') % 2);
-        System.out.println(count);
-        for (Map.Entry<Character, Integer> entry : count.entrySet()){
+        int total = 0;
+        total += 130 * (skuCount.get('A') / 3);
+        total += 45 * (skuCount.get('B') / 2);
+
+        skuCount.put('A', skuCount.get('A') % 3);
+        skuCount.put('B', skuCount.get('A') % 2);
+        System.out.println(skuCount);
+        for (Map.Entry<Character, Integer> entry : skuCount.entrySet()){
             total += entry.getValue() * prices.get(entry.getKey());
         }
 
-
         return total;
     }
+
+    private void buildPrices(){
+        prices.put('A', 50);
+        prices.put('B', 30);
+        prices.put('C', 20);
+        prices.put('D', 15);
+    }
 }
-
-
-
-
-
-
